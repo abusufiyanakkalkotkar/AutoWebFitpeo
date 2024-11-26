574d669149d2b11379910f1586f5a8be713fd15b
Abusufiyan Akkalkotkar <98733918+abusufiyanakkalkotkar@users.noreply.github.com>
2024-11-26 20:05:40 +0530

    FitPeoAutomation.java

--- src/main/java/automation/FitPeoAutomation.java
+++ src/main/java/automation/FitPeoAutomation.java
@@ -19,36 +19,36 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class FitPeoAutomation {
-    WebDriver driver;
+	WebDriver driver;
 
-    @BeforeClass
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
 
-    @Test(priority = 1)
-    public void navigateToHomepage() throws InterruptedException {
-    	// Navigating to FitPeo Home page
-        driver.get("https://fitpeo.com"); 
-        Thread.sleep(1000);
-    }
+	@Test(priority = 1)
+	public void navigateToHomepage() throws InterruptedException {
+		// Navigating to FitPeo Home page
+		driver.get("https://fitpeo.com");
+		Thread.sleep(1000);
+	}
 
-    @Test(priority = 2)
-    public void navigateToRevenueCalculatorPage() throws InterruptedException{
-    	// Navigating to the Revenue Calculator Page
-        WebElement calculatorLink = driver.findElement(By.xpath("//div[text()='Revenue Calculator']"));
-        calculatorLink.click();
-        Thread.sleep(1000);
-    }
+	@Test(priority = 2)
+	public void navigateToRevenueCalculatorPage() throws InterruptedException {
+		// Navigating to the Revenue Calculator Page
+		WebElement calculatorLink = driver.findElement(By.xpath("//div[text()='Revenue Calculator']"));
+		calculatorLink.click();
+		Thread.sleep(1000);
+	}
 
-    @Test(priority = 3)
-    public void adjustSliderAndUpdateTextField() throws InterruptedException{
-    	// Scrolling Down to the Slider section
+	@Test(priority = 3)
+	public void adjustSliderAndUpdateTextField() throws InterruptedException {
+		// Scrolling Down to the Slider section
 		JavascriptExecutor jx = (JavascriptExecutor) driver;
 		jx.executeScript("window.scrollBy(0,300)");
 		Thread.sleep(1000);
-		
+
 		// Adjusting the Slider to 820
 		Actions actions = new Actions(driver);
 		for (int i = 1; i <= 13; i++) {
@@ -57,7 +57,7 @@ public class FitPeoAutomation {
 		jx.executeScript("window.scrollBy(0,300)");
 		for (int i = 1; i <= 620; i++) {
 			actions.sendKeys(Keys.ARROW_RIGHT).perform();
-		}   
+		}
 		WebElement sliderBar = driver.findElement(By.xpath("//span[@class='MuiSlider-rail css-3ndvyc']"));
 		WebElement sliderHilightedBar = driver.findElement(By.xpath("//span[@class='MuiSlider-track css-10opxo5']"));
 		WebElement sliderPoint = driver.findElement(By.xpath("//span[@data-index='0']"));
@@ -65,8 +65,8 @@ public class FitPeoAutomation {
 		jx.executeScript("arguments[0].style.background = 'red';", sliderHilightedBar);
 		jx.executeScript("arguments[0].style.background = 'red';", sliderPoint);
 		Thread.sleep(1000);
-		
-        // Updating the text field to 560
+
+		// Updating the text field to 560
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement textField = driver.findElement(By.xpath("//input[@type='number'][1]"));
 		wait.until(ExpectedConditions.elementToBeClickable(textField)); // Replace with actual ID or locator
@@ -75,48 +75,48 @@ public class FitPeoAutomation {
 		textField.clear();
 		textField.clear();
 		Thread.sleep(2000);
-        textField.sendKeys("560");
-        JavascriptExecutor ji = (JavascriptExecutor) driver;
-        ji.executeScript("arguments[0].value='560';", textField);
-        //System.out.println("Entered value: " + textField.getAttribute("value"));
-        Thread.sleep(2000);
-        }
+		textField.sendKeys("560");
+		JavascriptExecutor ji = (JavascriptExecutor) driver;
+		ji.executeScript("arguments[0].value='560';", textField);
+		//System.out.println("Entered value: " + textField.getAttribute("value"));
+		Thread.sleep(2000);
+	}
 
-    @Test(priority = 4)
-    public void selectCPTCodesAndValidateReimbursement() throws InterruptedException {
-        // Scroll to CPT codes section
-    	JavascriptExecutor jp=(JavascriptExecutor)driver;
+	@Test(priority = 4)
+	public void selectCPTCodesAndValidateReimbursement() throws InterruptedException {
+		// Scroll to CPT codes section
+		JavascriptExecutor jp=(JavascriptExecutor)driver;
 		jp.executeScript("window.scrollBy(0,400)");
 		Thread.sleep(1000);
 
 		// Selecting CPT Codes
 		List<WebElement> cptCodes=driver.findElements(By.cssSelector("div.css-1p19z09> *>p:first-of-type"));
-        List<WebElement> checkBoxes=driver.findElements(By.cssSelector("div.css-1p19z09> *>label input"));
-        for (int i=0;i<cptCodes.size();i++){
-        	String code=cptCodes.get(i).getText();
-            if(code.equals("CPT-99091") || code.equals("CPT-99453")||code.equals("CPT-99454")||code.equals("CPT-99474")){
-                checkBoxes.get(i).click();
-                Thread.sleep(1000);
-            }
-        }
-        Thread.sleep(2000);
-        
-        // Validating Total Recurring Reimbursement
-        WebElement totalRecurringReimbursement=driver.findElement(By.cssSelector("div.css-m1khva>p:nth-of-type(2)"));
-        String total= totalRecurringReimbursement.getText();
-        String expecectedTotal="$110700";
-        if(total.equals(expecectedTotal)){
-            System.out.println(true);
-        }else{
-            System.out.println(false);
-        }
-        Thread.sleep(2000);
-    }
+		List<WebElement> checkBoxes=driver.findElements(By.cssSelector("div.css-1p19z09> *>label input"));
+		for (int i=0; i<cptCodes.size(); i++) {
+			String code=cptCodes.get(i).getText();
+			if(code.equals("CPT-99091") || code.equals("CPT-99453")||code.equals("CPT-99454")||code.equals("CPT-99474")) {
+				checkBoxes.get(i).click();
+				Thread.sleep(1000);
+			}
+		}
+		Thread.sleep(2000);
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		// Validating Total Recurring Reimbursement
+		WebElement totalRecurringReimbursement=driver.findElement(By.cssSelector("div.css-m1khva>p:nth-of-type(2)"));
+		String total= totalRecurringReimbursement.getText();
+		String expecectedTotal="$110700";
+		if(total.equals(expecectedTotal)) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+		Thread.sleep(2000);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
